48108d3c8f9f50df1c40d281574e9b1a81ccdeb7
Kyle Treleaven <treleaven.kyle@gmail.com>
2014-04-19 11:25:00 -0400

    incorporating new 'vehicles' mode for experiment generation and results analysis

--- scripts/generateExperiments.py
+++ scripts/generateExperiments.py
@@ -1,28 +1,61 @@
 #!/usr/bin/python
 
+def parseints( inputstr ) :
+    selection = set()
+    invalid = set()
+    
+    tokens = inputstr.split(',')
+    for i in tokens :
+        try :
+            selection.add( int(i) )
+        
+        except :
+            try :
+                token = [ int( k.strip() ) for k in i.split('-') ]
+                assert len( token ) == 2
+                
+                selection.update( xrange( token[0], token[1] + 1 ) )
+            
+            except :
+                invalid.add( i )
+                
+    if len( invalid ) > 0 : print invalid
+    return selection
+
 
 
 
 if __name__ == '__main__' :
     import sys
     import argparse
-    import setiptah.taxitheory.db.sql as experimentdb
-    
-    import numpy as np
-    #import matplotlib.pyplot as plt
     
     parser = argparse.ArgumentParser()
+    
+    # things which remain general
     parser.add_argument( '--dbfile', type=str, default='mydata.sqlite' )
     parser.add_argument( '--cleardb', action='store_true' )
-    
-    parser.add_argument( '--numveh', type=int, default=1 )
     parser.add_argument( '--vehspeed', type=float, default=1. )
     parser.add_argument( '--distr', type=str, default='PairUniform2' )
     parser.add_argument( '--policy', type=str, default='STACKERCRANE')
-    
+    # now, irrelevant
     parser.add_argument( '--phasedil', type=float, default=2. )
     
-    parser.add_argument( '--linterp', action='store_true' )
+    
+    # for sub-commands:
+    subp = parser.add_subparsers(dest='command')
+    
+    # for a range of orders
+    order_parser = subp.add_parser('orders')
+    order_parser.add_argument( '--ordermin', type=float, default=1. )
+    order_parser.add_argument( '--ordermax', type=float, default=10. )
+    order_parser.add_argument( '--linterp', action='store_true' )
+    order_parser.add_argument( 'N', type=int )
+    order_parser.add_argument( '--numveh', type=int, default=1 )
+    # for a range of fleet sizes
+    veh_parser = subp.add_parser('vehicles')
+    veh_parser.add_argument( '--torder', type=float, default=100. )
+    veh_parser.add_argument( 'sizes', type=str )
+    
     
     # etc...
     INITDUR = 50.
@@ -30,54 +63,91 @@ if __name__ == '__main__' :
     THRESHFACTOR = 1.1
     EXPLOITRATIO = 2.
     
-    parser.add_argument( 'N', type=int )
-    parser.add_argument( '--ordermin', type=float, default=1. )
-    parser.add_argument( '--ordermax', type=float, default=10. )
     
     
     
     """ start script """
     args, unknown_args = parser.parse_known_args()
     
-    import setiptah.taxitheory.distributions as distribs
-    distr = distribs.distributions[ args.distr ]
-    
-    if args.linterp :
-        G = np.linspace( args.ordermin, args.ordermax, args.N )
-    else :
-        G = np.logspace( np.log10( args.ordermin ), np.log10( args.ordermax ),
-                         args.N )
-    
-    RHO = [ distr.inverseQueueLengthFactor(g) for g in G ]
-    
-    moverscplx = distr.meancarry() + distr.meanfetch()
-    RATES = [ rho * args.numveh * args.vehspeed / moverscplx for rho in RHO ]
-    
-    db = experimentdb.ExperimentDatabase( args.dbfile, args.cleardb )
-    
-    from setiptah.taxitheory.db import ExperimentRecord
-    for rate in RATES :
+    def default_experiment() :
         e = ExperimentRecord()
-        
-        e.setDistribution( args.distr )
-        e.setFleetSize( args.numveh )
-        e.setFleetSpeed( args.vehspeed )
-        
-        #e.setRate( rate )
-        e.setRate( float(rate) )        # sympy is being annoying
-        
+        # all irrelevant
         e.setInitialDuration( INITDUR )
         e.setTimeDilation( args.phasedil )
         e.setGrowthThreshold( THRESHFACTOR )
         e.setExploitRatio( EXPLOITRATIO )
         
-        e.setPolicy( args.policy )
+        e.setDistribution( args.distr )
+        e.setPolicy( args.policy )      # fairly irrelevant
+        e.setFleetSpeed( args.vehspeed )
         
-        db.addExperiment( e )
+        #e.setFleetSize( args.numveh )
+        #e.setRate( float(rate) )        # sympy is being annoying
         
-
-
-
-
-
-
+        return e
+    
+    # import distributions
+    import setiptah.taxitheory.distributions as distribs
+    distr = distribs.distributions[ args.distr ]
+    moverscplx = distr.meancarry() + distr.meanfetch()
+    
+    # import database interfaces
+    import setiptah.taxitheory.db.sql as experimentdb
+    db = experimentdb.ExperimentDatabase( args.dbfile, args.cleardb )
+    
+    # utilities
+    from setiptah.taxitheory.db import ExperimentRecord
+    import numpy as np
+    
+    
+    """ dispatch the right sub-command """
+    if args.command == 'orders' :
+        # construct order-range sim
+        
+        if args.linterp :
+            G = np.linspace( args.ordermin, args.ordermax, args.N )
+        else :
+            G = np.logspace( np.log10( args.ordermin ), np.log10( args.ordermax ),
+                             args.N )
+            
+        RHO = [ distr.inverseQueueLengthFactor(g) for g in G ]
+        
+        RATES = [ rho * args.numveh * args.vehspeed / moverscplx for rho in RHO ]
+        
+        for rate in RATES :
+            e = default_experiment()
+            e.setFleetSize( args.numveh )
+            e.setRate( float(rate) )
+            
+            db.addExperiment( e )
+            
+    elif args.command == 'vehicles' :
+        # method to construct fleet-size range sim
+        NUMVEH = sorted( parseints( args.sizes ) )
+        
+        ORDERS = [ numveh * args.vehspeed * args.torder for numveh in NUMVEH ]
+        UTILS = [ distr.inverseQueueLengthFactor(o) for o in ORDERS ]
+        RATES = [ rho * numveh * args.vehspeed / moverscplx
+                 for rho, numveh in zip( UTILS, NUMVEH ) ]
+        
+        for numveh, rate in zip( NUMVEH, RATES ) :
+            e = default_experiment()
+            e.setFleetSize( numveh )
+            e.setRate( float(rate) )
+            
+            db.addExperiment( e )
+            
+    else :
+        raise Exception('sub-command not recognized')
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+            
+           
--- scripts/plotVsNumveh.py
+++ scripts/plotVsNumveh.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
-from PyQt4 import QtGui as gui, QtCore as core
-
-
-import setiptah.taxitheory.gui.mplcanvas as mplcanvas
+#from PyQt4 import QtGui as gui, QtCore as core
+#import setiptah.taxitheory.gui.mplcanvas as mplcanvas
 
 # simulation components
 #from setiptah.eventsim.signaling import Signal
@@ -51,17 +49,17 @@ if __name__ == '__main__' :
     
     # fixed variables
     parser.add_argument( 'distr', type=str, default='PairUniform2' )
-    parser.add_argument( '--numveh', type=int, default=1 )
+    #parser.add_argument( '--numveh', type=int, default=1 )
     parser.add_argument( '--vehspeed', type=float, default=1. )
     
     parser.add_argument( '--nonum', action='store_true' )
     parser.add_argument( '--nobound', action='store_true' )
     
-    parser.add_argument( '--warp', type=float, default=None )
-    parser.add_argument( '--warpnum', type=int, default=5 )
+    #parser.add_argument( '--warp', type=float, default=None )
+    #parser.add_argument( '--warpnum', type=int, default=5 )
     
-    parser.add_argument( '--fit', action='store_true' )
-    #parser.add_argument( '--fitwarp', action='store_true' )
+    parser.add_argument( '--fit', action='store_true' )     # we'll fill this in later
+    
     
     args, unknown_args = parser.parse_known_args()
     
@@ -71,20 +69,14 @@ if __name__ == '__main__' :
     
     INCLUDE = [ e for e in db.experimentsIter()
                if e.distrib_key == args.distr
-               and e.numveh == args.numveh
+               #and e.numveh == args.numveh
                and e.vehspeed == args.vehspeed ]
     #eiter = db.experimentsIter()
+    SIZES = [ e.numveh for e in INCLUDE ]
     
     import setiptah.taxitheory.distributions as distribs
     distr = distribs.distributions[ args.distr ]
-    
     mcplx_star = distr.meanfetch() + distr.meancarry()
-    mcplx_star /= args.numveh
-    mcplx_star /= args.vehspeed
-    
-    utils = [ mcplx_star * e.arrivalrate for e in INCLUDE ]
-    orders = [ distr.queueLengthFactor(rho) for rho in utils ]
-    orders = [ float(o) for o in orders ]
     
     def computeAverageSystemTime( e ) :
         demands = db.demandsIter( e.uniqueID )
@@ -99,25 +91,16 @@ if __name__ == '__main__' :
     if True :
         
         # show lines
-        plt.scatter( orders, meansys )
+        plt.scatter( SIZES, meansys )
         
         # find a trend line for the results?
         if args.fit :
             import scipy.stats as stats
             
-            print zip( orders, meansys )
-            print stats.linregress( orders, meansys )
-            
-            slope, intercept, r_value, _, __ = stats.linregress( orders, meansys )
-            fit = [ intercept + slope*o for o in orders ]
-            plt.plot( orders, fit, '--' )
-            
-            print 'slope: %.3f' % slope
-            print 'intercept: %.3f' % intercept
-            print 'r-value: %.3f' % r_value
+            pass
             
         if not args.nonum :
-            for x,y,label in zip( orders, meansys, labels ) :
+            for x,y,label in zip( SIZES, meansys, labels ) :
                 plt.annotate( label,
                               xy = (x, y), xytext = (-5, 5),
                               textcoords = 'offset points', ha = 'right', va = 'bottom',
@@ -130,67 +113,35 @@ if __name__ == '__main__' :
             bounds = {}
         else :
             bounds = distr.boundConstants()
+            
+            
+        # assume all orders are the same... generated that way...
+        e0 = INCLUDE[0]
+        rho = e0.arrivalrate * mcplx_star / e0.numveh / e0.vehspeed
+        order_star = distr.queueLengthFactor(rho)
         
-        # obtain range of mistake complexities
-        MCPLX = [ mcplx_star ]
-        if args.warp is not None :
-            alpha = args.warp
-            MCPLX.extend( mcplx_star * np.linspace( 1+alpha, 1-alpha, args.warpnum ) )
-            
-        for mult in MCPLX :
-            utils_alt = [ mult * e.arrivalrate for e in INCLUDE ]   # *actual* utilization
-            orders_alt = [ distr.queueLengthFactor(rho) for rho in utils_alt ]
-            
-            # show the trend? warped?
-            if args.fit :
-                line = [ intercept + slope * x for x in orders ]
-                plt.plot( orders_alt, line, '--' )
-            
-            # show the bounds (warped?)
-            for c in bounds.itervalues() :
-                line = [ c * x for x in orders ]
-                plt.plot( orders_alt, line, '--' )
-            
+        # show the trend? warped?
+        if args.fit :
+            pass    # later
+        
+        # show the bounds (warped?)
+        for c in bounds.itervalues() :
+            line = [ c * order_star / numveh / args.vehspeed for numveh in SIZES ]
+            plt.plot( SIZES, line, '--' )
+        
             
     else :          # show levels
-        plt.scatter( orders, [ st / n for n, st in zip( orders, meansys ) ] )
-        
-        if not args.nonum :
-            for x,y,label in zip( orders, meansys, labels ) :
-                plt.annotate( label, 
-                              xy = (x, y/x), xytext = (-5, 5),
-                              textcoords = 'offset points', ha = 'right', va = 'bottom',
-                              #bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
-                              #arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
-                              )
-            
-        bounds = distr.boundConstants()
-        for c in bounds.itervalues() :
-            line = [ c for x in orders ]
-            plt.plot( orders, line, '--' )
+        pass
+    
     
     # axes
+    RATES = [ e.arrivalrate for e in INCLUDE ]
+    ticklabels = [ '%.3f' % rate for rate in RATES ]
+    
     ax1 = plt.gca()
     ax2 = ax1.twiny()   # for utilization
-    
-    o1, o2 = ax1.get_xlim()
-    o1 = max(1.,o1)
-    #print o1, o2
-    numticks = 6
-    if False :
-        rho1 = distr.inverseQueueLengthFactor(o1)
-        rho2 = distr.inverseQueueLengthFactor(o2)
-        rhoticks = np.linspace(rho1,rho2, numticks+2 )[1:-1]
-        ticks = [ distr.queueLengthFactor(rho) for rho in rhoticks ]
-        
-    else :
-        ticks = np.linspace(o1,o2, numticks+2 )[1:-1]
-        rhoticks = [ distr.inverseQueueLengthFactor(o) for o in ticks ]
-        
-    ticklabels = [ '%.3f' % rho for rho in rhoticks ]
-    ax2.set_xticks( ticks )
+    ax2.set_xticks( SIZES )
     ax2.set_xticklabels( ticklabels )
-    #ax2.set_xlabel(r"Modified x-axis: $1/(1+X)$")
     
     xlabel = distr.horizontalAxisLabel()
     if xlabel is not None :
